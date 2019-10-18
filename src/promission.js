94ad077ef7cc2cec4960dfea7ede87816c45b9b8
赵云 <zzlan@shinyway.com.cn>
2019-10-18 14:55:35 +0800

    修改路由表从easy-mock请求变为模拟请求

--- src/promission.js
+++ src/promission.js
@@ -1,23 +1,89 @@
 import router from './router'
 // import { Message } from 'element-ui'
 import axios from 'axios'
-const _import = require('./router/_import_' + process.env.NODE_ENV)//获取组件的方法
+const _import = require('./router/_import_' + process.env.NODE_ENV) //获取组件的方法
 import Layout from '@/views/layout' //Layout 是架构组件，不在后台返回，在文件里单独引入
 
 
 var getRouter //用来获取后台拿到的路由
 
-router.beforeEach((to, from, next) => {
-  if (!getRouter) {//不加这个判断，路由会陷入死循环
-    if (!getObjArr('router')) {
-      axios.get('https://www.easy-mock.com/mock/5a5da330d9b48c260cb42ca8/example/antrouter').then(res => {
-        getRouter = res.data.data.router//后台拿到路由
-        saveObjArr('router', getRouter) //存储路由到localStorage
+// 假装fakeRouter是通过后台接口请求回来的数据
+let fakeRouter = {
+  "router": [{
+      "path": "",
+      "component": "Layout",
+      "redirect": "dashboard",
+      "children": [{
+        "path": "dashboard",
+        "component": "dashboard/index",
+        "meta": {
+          "title": "首页",
+          "icon": "dashboard"
+        }
+      }]
+    },
+    {
+      "path": "/example",
+      "component": "Layout",
+      "redirect": "/example/table",
+      "name": "Example",
+      "meta": {
+        "title": "案例",
+        "icon": "example"
+      },
+      "children": [{
+          "path": "table",
+          "name": "Table",
+          "component": "table/index",
+          "meta": {
+            "title": "表格",
+            "icon": "table"
+          }
+        },
+        {
+          "path": "tree",
+          "name": "Tree",
+          "component": "tree/index",
+          "meta": {
+            "title": "树形菜单",
+            "icon": "tree"
+          }
+        }
+      ]
+    },
+    {
+      "path": "/form",
+      "component": "Layout",
+      "children": [{
+        "path": "index",
+        "name": "Form",
+        "component": "form/index",
+        "meta": {
+          "title": "表单",
+          "icon": "form"
+        }
+      }]
+    },
+    {
+      "path": "*",
+      "redirect": "/404",
+      "hidden": true
+    }
+  ]
 
-        routerGo(to, next)//执行路由跳转方法
-      })
-    } else {//从localStorage拿到了路由
-      getRouter = getObjArr('router')//拿到路由
+}
+router.beforeEach((to, from, next) => {
+  if (!getRouter) { //不加这个判断，路由会陷入死循环
+    if (!getObjArr('router')) {
+      // easy-mock官网经常挂掉，所以就不请求了,你们可以替换成自己公司的接口去请求,把下方的axios请求打开即可
+      // axios.get('https://www.easy-mock.com/mock/5a5da330d9b48c260cb42ca8/example/antrouter').then(res => {
+      getRouter = fakeRouter.router //假装模拟后台请求得到的路由数据
+      saveObjArr('router', getRouter) //存储路由到localStorage
+
+      routerGo(to, next) //执行路由跳转方法
+      // })
+    } else { //从localStorage拿到了路由
+      getRouter = getObjArr('router') //拿到路由
       routerGo(to, next)
     }
   } else {
@@ -46,7 +112,7 @@ function getObjArr(name) { //localStorage 获取数组对象的方法
 function filterAsyncRouter(asyncRouterMap) { //遍历后台传来的路由字符串，转换为组件对象
   const accessedRouters = asyncRouterMap.filter(route => {
     if (route.component) {
-      if (route.component === 'Layout') {//Layout组件特殊处理
+      if (route.component === 'Layout') { //Layout组件特殊处理
         route.component = Layout
       } else {
         route.component = _import(route.component)
